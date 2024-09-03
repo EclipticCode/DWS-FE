569052b3a0f5041c4bcf0100534639b4550f6ef2
daylightcoderj6 <jananibharathi6@gmail.com>
2024-09-03 08:46:16 +0530

    added Logout scenario

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,16 @@ import { Link } from "react-router-dom";
 import logo from '../assets/Logo.png'
 
 const Navbar = () => {
+
+  const username = localStorage.getItem('login')
+
+  const handleLogout = () => {
+    localStorage.setItem('login' , "")
+    localStorage.setItem('token' , "")
+    window.location.reload()
+    alert("Logout Successful")
+  }
+
   return (
     <div className="md:block lg:flex justify-between items-center px-4 py-2 bg-navbar drop-shadow-md">
       <div><img src={logo} alt="" className="h-14 w-14 rounded-full" /></div>
@@ -17,10 +27,12 @@ const Navbar = () => {
           <Link to={"/aboutUs"}>About us</Link>
         </div>
       </div>
-      <div>
-        <button className="border-2 text-primary border-primary rounded-md px-3 py-1 hover:bg-primary hover:text-white">
-          <Link to={'/login'}>Login</Link>
-        </button>
+      <div className="px-8">
+       {username ?  <button className="border-2 text-primary border-primary rounded-md px-3 py-1 hover:bg-primary hover:text-white" onClick={() => handleLogout()}>
+         Logout
+        </button> :  <button className="border-2 text-primary border-primary rounded-md px-3 py-1 hover:bg-primary hover:text-white">
+         <Link to={'/login'}>Login</Link>
+        </button>}
       </div>
     </div>
   );
